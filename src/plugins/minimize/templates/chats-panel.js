0b89af0713024d041f67c1a13c3d7c0f7469ebd2
danieldiazmc <danieldiazmc@gmail.com>
2021-09-22 08:48:18 -0300

    Se configuran tags de chats minimizados

--- src/plugins/minimize/templates/chats-panel.js
+++ src/plugins/minimize/templates/chats-panel.js
@@ -1,9 +1,9 @@
 import { html } from "lit";
 import { __ } from 'i18n';
 
-export default (o) =>
-    html`<div id="minimized-chats" class="${o.chats.length ? '' : 'hidden'}">
-        <a id="toggle-minimized-chats" class="row no-gutters" @click=${o.toggle}>
+export default (o) =>{
+    return html`<div id="minimized-chats" class="${o.chats.length ? '' : 'hidden'}">
+        <a id="toggle-minimized-chats" class="row no-gutters hidden" @click=${o.toggle}>
             ${o.num_minimized} ${__('Minimized')}
             <span class="unread-message-count ${!o.num_unread ? 'unread-message-count-hidden' : ''}" href="#">${o.num_unread}</span>
         </a>
@@ -11,8 +11,8 @@ export default (o) =>
             ${o.chats.map(chat =>
                 html`<converse-minimized-chat
                         .model=${chat}
-                        title=${chat.getDisplayName()}
+                        title=${chat.get('nickname')}
                         type=${chat.get('type')}
                         num_unread=${chat.get('num_unread')}></converse-minimized-chat>`)}
         </div>
-    </div>`;
+    </div>`};
