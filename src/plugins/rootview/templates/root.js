b877b851ed3822e2f26c1d873a48346f4b607bdd
danieldiazmc <danieldiazmc@gmail.com>
2021-09-24 09:39:58 -0300

    Se alinean todos los box al pie de la pantalla

--- src/plugins/rootview/templates/root.js
+++ src/plugins/rootview/templates/root.js
@@ -6,7 +6,7 @@ export default () => {
     let extra_classes = api.settings.get('singleton') ? 'converse-singleton' : '';
     extra_classes += `converse-${api.settings.get('view_mode')}`;
     return html`
-        <converse-chats class="converse-chatboxes row no-gutters ${extra_classes}"></converse-chats>
+        <converse-chats class="converse-chatboxes no-gutters ${extra_classes}"></converse-chats>
         <div id="converse-modals" class="modals"></div>
         <converse-fontawesome></converse-fontawesome>
     `;
